edd210014767c1d371fa3daae254400a1de38566
jay <jayped007@gmail.com>
2020-10-04 10:01:46 -0500

    4.4 first JS implementation

--- www/index.js
+++ www/index.js
@@ -1,3 +1,20 @@
-import * as wasm from "wasm-game-of-life";
+import { Universe } from "wasm-game-of-life";
 
-wasm.greet("Jack");
+// get 'pre' canvas element from index.html (centered)
+const pre = document.getElementById("game-of-life-canvas");
+
+// create Universe (via wasm)
+const universe = Universe.new();
+
+// iteration callback -- requestAnimationFrame every tick and update Universe
+const renderLoop = () => {
+  pre.textContent = universe.render(); // current universe -- rendered via Display trait from Rust wasm -- shows square boxes
+  universe.tick(); // update Universe (seen on next tick), defined in Rust wasm
+
+  requestAnimationFrame(renderLoop); // infinite loop
+
+  // Do frames pile up? No.  This frame doesn't wait for next iteration of renderloop.  It requested it and its done.
+};
+
+// start rendering
+requestAnimationFrame(renderLoop);
