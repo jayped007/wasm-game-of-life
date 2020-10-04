7d4505844adb68041673c14581566a73c07b58a2
jay <jayped007@gmail.com>
2020-10-04 07:46:43 -0500

    4.4 but not working yet

--- src/lib.rs
+++ src/lib.rs
@@ -8,12 +8,157 @@ use wasm_bindgen::prelude::*;
 #[global_allocator]
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
+// Define a cell for the 'Universe', each 1 byte
+//   use repr(u8) to ensure 1 byte unsigned values
+//
+//   NOTE: Define Dead value as zero and alive as one allow simple summing
+//   to determine how many live cells.
 #[wasm_bindgen]
-extern {
-    fn alert(s: &str);
+#[repr(u8)]
+#[derive(Clone, Copy, Debug, PartialEq)]
+pub enum Cell {
+    Dead = 0,
+    Alive = 1
 }
 
+// Define the 'Universe', a 1D array of Cell values (byte values, 0 or 1 per Cell def)
+//   Give the width of the universe, each row of the universe is the next set
+//   of 'width' cells, starting with the first row from indexes 0:<width>
 #[wasm_bindgen]
-pub fn greet(name: &str) {
-    alert(format!("Hello, {}!", name).as_str());
+pub struct Universe {
+    width:  u32,  // width of each row
+    height: u32,  // number of rows
+    cells:  Vec<Cell>  // width*height cells, each one byte
+}
+
+// implement Display trait for Universe
+//   used by to_string() method used later in render() method
+//   Algo:
+//     Write 'height' lines, each line representing the next row in Universe
+//     for each column in a row show closed square box if live else an open square box.
+
+use std::fmt;
+
+impl fmt::Display for Universe
+{
+
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        for line in self.cells.as_slice().chunks(self.width as usize) {
+            for &cell in line
+            {
+                let symbol = if cell == Cell::Dead { '◻' } else { '◼' };
+                write!(f, "{}", symbol)?;
+            }
+            write!(f, "\n");
+        }
+        Ok(())
+    }
+
+}
+
+// methods for Universe, but not exposed to JS
+impl Universe
+{
+    // get_index - Return 1D array index of Cell at position (row,column) in Universe
+    fn get_index(&self, row: u32, column: u32) -> usize
+    {
+        (row * self.width + column) as usize
+    }
+
+    // Count live neighbors of cell at (row, column)
+    fn live_neighbor_count(&self, row: u32, column: u32) -> u8
+    {
+        let mut neighbors = 0;
+        // Examine cells surrounging (row, column).  This is done by looking
+        // at rows and columns at relative positions -1,0,0 from (row,colum).
+        // NOTE: No special processing for 'last row' or 'last column'?
+        //       modulus wraps the value to the 'other side of the universe'?
+        for delta_row in [self.height -1, 0, 1].iter().cloned()
+        {
+            for delta_col in [self.width -1, 0, 1].iter().cloned()
+            {
+                if delta_row == 0 && delta_col == 0 {
+                    continue; // ignore self
+                }
+                let neighbor_row = (row + delta_row) % self.height;
+                let neighbor_col = (column + delta_col) % self.width;
+                let cell_idx = self.get_index(neighbor_row, neighbor_col);
+                if self.cells[cell_idx] == Cell::Alive {
+                    neighbors += 1;
+                }
+            }
+        }
+        neighbors
+    }
+    
+}
+
+// Public methods, exposed to JS
+#[wasm_bindgen]
+impl Universe
+{
+    pub fn tick(&mut self)
+    {
+        let mut next = self.cells.clone(); // copy of current cells, modify ==> next state
+
+        // Determine next state of Universe by applying conways' 4 rules
+        for row in 0..self.height {
+            for col in 0..self.width {
+                let idx = self.get_index(row, col);
+                let cell = self.cells[idx]; // Cell::Alive (1), or Dead (0)
+                let neighbors = self.live_neighbor_count(row, col);
+
+                let next_cell = match (cell, neighbors)
+                {
+                    // Rule 1: any live cell with < 2 live neighbors dies, (loneliness)
+                    (Cell::Alive, x) if x < 2 => Cell::Dead,
+                    // Rule 2: any live cell with 2 to 3 live neighbors continues to live (stable)
+                    (Cell::Alive, 2) | (Cell::Alive, 3) => Cell::Alive,
+                    // Rule 3: any live cell with > 3 live neighbors dies (overpopulation)
+                    (Cell::Alive, x) if x > 3 => Cell::Dead,
+                    // Rule 4: any dead cel with = 3 live neighbors comes alive (reproduction)
+                    (Cell::Dead, 3) => Cell::Alive,
+                    // Otherwise -- no change
+                    (otherwise, _) => otherwise
+                };
+                next[idx] = next_cell;
+            }
+        }
+        self.cells = next; // next state for Universe determined
+    }
+
+    // Constructor, initialize the universe to hard-coded pattern
+    pub fn new() -> Universe
+    {
+        let width = 64;
+        let height = 64;
+
+        // hardcoded pattern, depends on 8x8 definition
+        //   use closure over the 1D array with zero-rel index i
+        let cells = (0..width * height).map(|i|
+        {
+            if i % 2 == 0 || i % 7 == 0 {
+                Cell::Alive
+            } else {
+                Cell::Dead
+            }
+        })
+        .collect();
+
+        Universe
+        {
+            width,
+            height,
+            cells,
+        }
+
+    }
+
+    // render() -- what does the string returned look like?
+    //   How is it defined what to_string() will do?
+    //   By the "Display" trait!
+    pub fn render(&self) -> String {
+        self.to_string()
+    }
+
 }
