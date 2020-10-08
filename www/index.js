6923021142908b5bb1106bd23d3b16e425a28239
jay <jayped007@gmail.com>
2020-10-07 21:47:30 -0500

    add frames-per-sec div

--- www/index.js
+++ www/index.js
@@ -3,6 +3,50 @@ import { Universe, Cell, InitialPattern } from "wasm-game-of-life";
 // Import the WebAssembly memory
 import { memory } from "wasm-game-of-life/wasm_game_of_life_bg";
 
+// FPS timer (frames per second) -- useful as we investigate speeding up our Game of Life's rendering.
+const fps = new (class {
+  constructor() {
+    this.fps = document.getElementById("fps");
+    this.frames = [];
+    this.lastFrameTimeStamp = performance.now();
+  }
+
+  render() {
+    // Convert the delta time since the last frame render into a measure
+    // of frames per second.
+    const now = performance.now();
+    const delta = now - this.lastFrameTimeStamp;
+    this.lastFrameTimeStamp = now;
+    const fps = (1 / delta) * 1000;
+
+    // Save only the latest 100 timings.
+    this.frames.push(fps);
+    if (this.frames.length > 100) {
+      this.frames.shift();
+    }
+
+    // Find the max, min, and mean of our 100 latest timings.
+    let min = Infinity;
+    let max = -Infinity;
+    let sum = 0;
+    for (let i = 0; i < this.frames.length; i++) {
+      sum += this.frames[i];
+      min = Math.min(this.frames[i], min);
+      max = Math.max(this.frames[i], max);
+    }
+    let mean = sum / this.frames.length;
+
+    // Render the statistics.
+    this.fps.textContent = `
+  Frames per Second:
+           latest = ${Math.round(fps)}
+  avg of last 100 = ${Math.round(mean)}
+  min of last 100 = ${Math.round(min)}
+  max of last 100 = ${Math.round(max)}
+  `.trim();
+  }
+})();
+
 const CELL_SIZE = 5; // px
 const GRID_COLOR = "#CCCCCC"; // grey
 const DEAD_COLOR = "#FFFFFF"; // white
@@ -145,6 +189,8 @@ canvas.addEventListener("mousedown", (evt) => {
 let animationId = null;
 
 const renderLoop = () => {
+  fps.render(); //new
+
   //debugger; // starts browser debugger, JS keyword, ECMAscript 1
   universe.tick(); // update Universe
 
